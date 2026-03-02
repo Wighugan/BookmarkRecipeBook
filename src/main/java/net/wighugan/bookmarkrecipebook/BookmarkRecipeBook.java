14097f75d1088c7261f162ca933bd49306032dff
Wighugan <amigingerr@gmail.com>
2026-03-02 21:28:50 +0700

    Initial commit

---
+++ src/main/java/net/wighugan/bookmarkrecipebook/BookmarkRecipeBook.java
@@ -0,0 +1,16 @@
+package net.wighugan.bookmarkrecipebook;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BookmarkRecipeBook implements ModInitializer {
+	public static final String MOD_ID = "bookmark-recipe-book";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/wighugan/bookmarkrecipebook/BookmarkRecipeBookClient.java
@@ -0,0 +1,10 @@
+package net.wighugan.bookmarkrecipebook;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BookmarkRecipeBookClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
